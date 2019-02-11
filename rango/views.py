78595aae64890f41f10d3f776de886e7d236d357
sj fang <2347600f@student.gla.ac.uk>
2019-02-11 18:12:26 +0000

    the completion 7 with exercise

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 # Create your views here
 from django.http import HttpResponse
 '''def index(request):
@@ -20,7 +21,7 @@ def about(request):
     context_dict = {'boldmessage': "This tutorial has been put together by Shijie Fang!"}
     return render(request,'rango/about.html',context=context_dict)
 
-def show_category(request, category_name_slug):
+def show_category(request,category_name_slug):
     context_dict = {}
     try:
         category = Category.objects.get(slug = category_name_slug)
@@ -32,7 +33,8 @@ def show_category(request, category_name_slug):
         context_dict['pages']=None
     return render(request, 'rango/category.html',context=context_dict)
 
-def add_category(request):
+def add_category(request ):
+
     form = CategoryForm()
 
     if request.method == 'POST':
@@ -45,3 +47,24 @@ def add_category(request):
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+    context_dict = {'form':form, 'category':category}
+    return render(request,'rango/add_page.html',context=context_dict)
