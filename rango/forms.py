a542bbd970eda31b75234f69415de0f8d1104930
sj fang <2347600f@student.gla.ac.uk>
2019-02-12 02:26:50 +0000

    the ch9.5

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category,UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -31,3 +32,21 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
             return cleaned_data
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+
+class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(required=False)
+    picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = ('website','picture')
+
+
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
