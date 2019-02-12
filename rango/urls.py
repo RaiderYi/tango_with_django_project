9f6b8e0e5e7014aa31dd7fe66dabeb6a8c15bca6
sj fang <2347600f@student.gla.ac.uk>
2019-02-12 01:38:20 +0000

    the ch8 completion

--- rango/urls.py
+++ rango/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
